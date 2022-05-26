00e7b5bc1134efe4f9e537387d39dfd2a7a16e04
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-05-26 12:34:19 +0300

    ganykla/gamykla

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
+import { v4 as uuidv4 } from 'uuid';
 import randColor from './Functions/randColor';
-import rand from './Functions/rand';
 
 
 function App() {
-  const [kv, setKv] = useState([]);
-  const [rt, setRt] = useState([]);
-  let aork =0;
-  const generuok =()=>{
-    for(let i=0;i<20;i++){
-    aork=rand(0,1);
-    if(aork == 0){
-      setRt(k => [...k, randColor()]);
-    }else{
-      setKv(k => [...k, randColor()]);
+
+    const [avys, setAvys] = useState([]);
+    const newA = () => {
+        const avis = {
+            id: uuidv4(),
+            color: randColor(),
+            where: 'ganykla'
+        }
+        setAvys(a => [...a, avis])
     }
-  }
-  }
-  const isvalyti = () => {
-    setKv([]);
-    setRt([]);
-}
-
-  const addRt = () => {
-    setRt(k => [...k, randColor()]);
-  }
-
-  const remRt = () => {
-    setRt(k => k.slice(1));
-  }
-
-
-  const addKv = () => {
-    setKv(k => [...k, randColor()]);
-  }
-
-  const remKv = () => {
-    setKv(k => k.slice(1));
-  }
-
-  
-  useEffect(() => {
-    setKv(JSON.parse(localStorage.getItem('kv') ?? '[]'));
-}, []);
-
-useEffect(() => {
-    if (null === kv) {
-        return;
-    }
-    localStorage.setItem('kv', JSON.stringify(kv));
-}, [kv]);
-
-
-useEffect(() => {
-  setKv(JSON.parse(localStorage.getItem('rt') ?? '[]'));
-}, []);
-
-useEffect(() => {
-  if (null === rt) {
-      return;
-  }
-  localStorage.setItem('rt', JSON.stringify(rt));
-}, [rt]);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={generuok}>generuok</button>
-        <button onClick={isvalyti}>paleisti</button>
-        <button onClick={addKv}>ADD []</button>
-        <button onClick={remKv}>REMOVE []</button>
-        <button onClick={addRt}>ADD O </button>
-        <button onClick={remRt}>REMOVE O</button>
-        <div>[Ganykla]</div>
-        <div className="kvc">
-          {
-            kv.map((c, i) => <div className="kv" key={i} style={{ backgroundColor: c }}>{""+c}</div>)
-          }
-           {
-            rt.map((c, i) => <div className="rt" key={i} style={{ backgroundColor: c }}>{''+c}</div>)
-          }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>PRAKTIMUMAS</h1>
+                <div className="kvc">
+                    {
+                        avys.map(a => <div key={a.id} className="kv"style={{background:a.color}}></div>)
+                    }
+                </div>
+                <button onClick={newA}>Nauja avis</button>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    )
 }
+
 export default App;
